83a46420aae6a1470bfa6c70da94e1a5fd7cd19d
Malware <4653628+malware-dev@users.noreply.github.com>
2026-02-20 21:45:27 +0100

    Added AlphaPaint for altering the alpha value of an existing paint

---
+++ libraries/Mal.MdkScriptMixin.Graphics/Mal.MdkScriptMixin.Graphics/AlphaPaint.cs
@@ -0,0 +1,25 @@
+﻿using VRageMath;
+
+namespace IngameScript
+{
+    public class AlphaPaint : IPaint
+    {
+        readonly byte _alpha;
+        readonly IPaint _basePaint;
+
+        public AlphaPaint(IPaint basePaint, byte alpha)
+        {
+            _basePaint = basePaint;
+            _alpha = alpha;
+        }
+
+        public AlphaPaint(IPaint basePaint, float alpha)
+        {
+            _basePaint = basePaint;
+            _alpha = (byte)(alpha * 255);
+        }
+
+        public Color Color => _basePaint.Color.Alpha(_alpha);
+        public string Texture => _basePaint.Texture;
+    }
+}
